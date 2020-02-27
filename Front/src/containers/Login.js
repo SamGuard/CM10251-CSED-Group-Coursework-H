ee064ad4d55b9a7ad9fa2554efd052363b84f4bf
Harry <harrykeartland@gmail.com>
2020-02-27 18:50:31 +0000

    made some tooltips

--- Front/src/containers/Login.js
+++ Front/src/containers/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import {OverlayTrigger, Tooltip, Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import "./Login.css";
 
 export default function Login(props) {
@@ -7,12 +7,12 @@ export default function Login(props) {
   const [password, setPassword] = useState("");
 
   function validateForm() {
-      return validateEmail() && validatePassword();
+    return validateEmail() && validatePassword();
   }
 
   function validateEmail() {
-      var pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/i;
-      return pattern.test(email)
+    var pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/i;
+    return pattern.test(email)
   }
 
   function validatePassword() {
@@ -28,25 +28,48 @@ export default function Login(props) {
       <form onSubmit={handleSubmit}>
         <FormGroup controlId="email" bsSize="large">
           <ControlLabel>Email: </ControlLabel>
-          <FormControl
-            autoFocus
-            type="email"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-          />
-        </FormGroup>
-        <FormGroup controlId="password" bsSize="large">
-          <ControlLabel>Password: </ControlLabel>
-          <FormControl
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-            type="password"
-          />
-        </FormGroup>
-        <Button block bsSize="large" disabled={!validateForm()} type="submit">
-          Login
-        </Button>
-      </form>
-    </div>
-  );
-}
+          <OverlayTrigger
+            key="right"
+            placement="right"
+            overlay={
+              <Tooltip id={`tooltip-right`}>
+                Emails must be of the form <strong>example@example.example</strong>
+              </Tooltip>
+            }
+            >
+              <FormControl
+                autoFocus
+                type="email"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                id="emailInput"
+              />
+            </OverlayTrigger>
+          </FormGroup>
+
+          <FormGroup id="passwordInput" controlId="password" bsSize="large">
+            <ControlLabel>Password: </ControlLabel>
+            <OverlayTrigger
+              key="right"
+              placement="right"
+              overlay={
+                <Tooltip id={`tooltip-right`}>
+                  Passwords must be a minimum of <strong>8</strong> characters.
+                </Tooltip>
+              }
+              >
+                <FormControl
+                  value={password}
+                  onChange={e => setPassword(e.target.value)}
+                  type="password"
+                />
+              </OverlayTrigger>
+            </FormGroup>
+
+            <Button block bsSize="large" disabled={!validateForm()} type="submit">
+              Login
+            </Button>
+          </form>
+        </div>
+      );
+    }
