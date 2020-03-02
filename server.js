fd443f5961be90f1dface036240c56760ea78149
SGuard27 <Guards27@hotmail.com>
2020-03-02 20:47:59 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,12 @@
-const https = require('https');
+const https = require('http');
 const fs = require('fs');
 const app = require('./app.js');
 const port = process.env.PORT || 3000;
 
-
-const options = {
-    key: fs.readFileSync('key.pem'),
-    cert: fs.readFileSync('cert.pem')
-};
-
-var server = https.createServer(options, app);
+var server = https.createServer(app);
 
 server.listen(port);
 
 console.log("server opened on port 3000");
-console.log("Go to https://localhost:3000 make sure you include the https otherwise it won't work");
+console.log("Go to localhost:3000");
 console.log("Your browser will probably warn you that the certificate is invalid or self signed, just click continue (trust me its safe)");
